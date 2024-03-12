288706c9af0a674b2e520077b80056f3a3d4a026
Eytan Anjel <eytananjel@Eytans-MBP-170.lan>
2024-03-12 00:28:05 -0700

    hardcoded e_key_command

--- app/e_key_command.go
+++ app/e_key_command.go
@@ -12,14 +12,21 @@ func (j EKeyCommand) Execute(m *Model) error {
 		return nil
 	}
 
-	filePath := m.FileManager.SelectedFile.FullPath
 	//editor := os.Getenv("EDITOR")
-	editor := "vim"
+	//if editor != "" {
+	//splitEditorArgs := strings.Split(editor, " ")
+	//if len(splitEditorArgs) > 1 {
+	//for _, arg := range splitEditorArgs[1:] {
+	//args = append(args, arg)
+	//}
+	//}
+	//}
 
-	if editor == "" {
-		editor = "vim" // Default to vim if $EDITOR is not set
-	}
-	cmd := exec.Command(editor, filePath)
+	//if editor == "" {
+	//editor = "vim" // Default to vim if $EDITOR is not set
+	//}
+	filePath := m.FileManager.SelectedFile.FullPath
+	cmd := exec.Command("vim", "-u", "~/.dotfiles/.vimrc", filePath)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Run()
